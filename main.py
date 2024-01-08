10c5c4ef58d7db4ab2fd5ea813db742ab7a3941a
CarlosDLCM <delamorenacoco.carlos@gmail.com>
2024-01-08 15:55:35 +0100

    pdf creating added

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from pdf_operations import get_words_by_subject
+from pdf_operations import get_words_by_subject, PDFConstructor
 from chatgpt_operations import get_translation
+
+import os
 
 
 RESULTS_PATH = "results_txt"
 
+# Getting all the words, separated by subject
 words_by_subject = get_words_by_subject("Der-deutsche-Wortschatz-von-A1-bis-B2-Lingster-Academy.pdf")
 
 print("total number of words: ", sum(len(words_list) for words_list in words_by_subject.values()))
 
-for subject in list(words_by_subject)[11:]:
+# Writing all the words into a .txt for every subject
+for subject in list(words_by_subject):
     file_name = subject.strip().replace(" ", "_") + ".txt"
     for i, word in enumerate(words_by_subject[subject]):
         if word.strip().isdigit():
@@ -18,3 +22,28 @@ for subject in list(words_by_subject)[11:]:
         print(f"Got translation for word {word}! ({i+1}/{len(words_by_subject[subject])}). Writing into {file_name}...")
         with open(f"{RESULTS_PATH}/{file_name}", "a") as file:
             file.write(f"'''\n{translation}\n")
+
+# Reading the .txt files and converting them to pdf
+for file in os.listdir(RESULTS_PATH):
+
+    with open(f"{RESULTS_PATH}/{file}", 'r') as f:
+        text = f.read()
+
+    words_with_info = text.split("'''\n")
+
+    file_name = file.replace(".txt", "")
+
+    constructor = PDFConstructor(file_name)
+    for word_with_info in words_with_info:
+        lines = word_with_info.split("\n", 4)
+        if len(lines) < 5:
+            print_lines = '\n'.join(lines)
+            print(f"This word has less than 5 lines: {print_lines}")
+            continue
+        german_word = lines[0].strip()
+        spanish_translation = lines[1].strip()
+        russian_translation = lines[2].strip()
+        english_translation = lines[3].strip()
+        sentences = lines[4]
+        constructor.add_word(german_word, spanish_translation, russian_translation, english_translation, sentences)
+    constructor.save_file()
--- pdf_operations.py
+++ pdf_operations.py
@@ -1,5 +1,10 @@
+# pip install reportlab PyPDF2
 from PyPDF2 import PdfReader
-
+from reportlab.platypus import Paragraph, SimpleDocTemplate, Spacer
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
 
 def get_words_by_subject(pdf_file_name):
     reader = PdfReader(pdf_file_name)
@@ -25,5 +30,52 @@ def get_words_by_subject(pdf_file_name):
             current_subject.append(word)
     words_by_subject[subject_name] = current_subject.copy()
     return words_by_subject
+
+
+class PDFConstructor:
+
+    def __init__(self, file_name):
+        pdfmetrics.registerFont(TTFont('DejaVuSerif', 'DejaVuSerif.ttf', 'UTF-8'))
+        self.doc = SimpleDocTemplate(f'./results_pdf/{file_name}.pdf',
+                                pagesize=A4,
+                                title=file_name
+        )
+        self.story = [Paragraph(file_name, self._styles_constructor('Heading1', 'center', font_size=20, bold=True))]
+
+    def _styles_constructor(self, base: str, alignment: str, bold: bool = False, font_size: int = 12):
+        alignment_dict = {
+            'left': 0,
+            'center': 1,
+            'right': 2
+        }
+        style = ParagraphStyle(
+            'Style',
+            parent=getSampleStyleSheet()[base],
+            alignment=alignment_dict[alignment],  # 0=left, 1=center, 2=right
+            fontName='Helvetica-Bold' if bold else 'DejaVuSerif',
+            fontSize=font_size
+        )
+        return style
+
+    def add_word(self, german_word, spanish_word, russian_word, english_word, sentences):
+        word_story = []
+        word_story.append(Paragraph(german_word, self._styles_constructor('Normal', 'left', font_size=14, bold=True)))
+        word_story.append(Spacer(1, 15))
+        word_story.append(Paragraph(spanish_word, self._styles_constructor('Normal', 'left')))
+        word_story.append(Spacer(1, 5))
+        word_story.append(Paragraph(russian_word, self._styles_constructor('Normal', 'left')))
+        word_story.append(Spacer(1, 5))
+        word_story.append(Paragraph(english_word, self._styles_constructor('Normal', 'left')))
+        word_story.append(Spacer(1, 15))
+        for sentence in sentences.split('\n'):
+            word_story.append(Paragraph(sentence, self._styles_constructor('Normal', 'left')))
+            word_story.append(Spacer(1, 5))
+
+        self.story += word_story
+        self.story.append(Spacer(1, 15))
+        # doc.build(word_story)
+
+    def save_file(self):
+        self.doc.build(self.story)
 
 
